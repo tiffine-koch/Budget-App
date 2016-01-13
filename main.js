38cb626767374902b4291d7e674abd3e7dac0696
Apple <tiffine.koch@gmail.com>
2016-01-13 11:02:13 -0800

    added dropdown functionality

--- main.js
+++ main.js
@@ -4,10 +4,10 @@ $(document).ready(init);
 
 function init() {
 
-  $('#formEntry').submit(newTrans);
-  $('#dataEntry').click(catEntry);
-  $('table').on('click', '#trash', deleteTrans);
   $('.dropList').on('click', findCat)
+  $('#formEntry').submit(newTrans);
+  $('#catDropDown').on('change', findCat);
+  $('table').on('click', '#trash', deleteTrans);
   console.log('load');
 
 }
@@ -26,16 +26,16 @@ function newTrans() {
   var formattedDate = moment(dateEntry).format('l');
   var amountEntry = $('#amountEntry').val();
   // var currentBalance =
-  var catEntry = $('#catEntry').val();
+  // var catEntry = $('ul.foo li:selected').val();
+  var catEntry = $('#catEntry').text();
 
   var $tr = $('#template').clone();
   $tr.removeAttr('id');
   $tr.children('.trans').text(transName);
   $tr.children('.date').text(formattedDate);
-  var $newAmount = $tr.children('.amount').text(amountEntry);
+  var $newAmount = $tr.children('.amount');
   // $tr.children('.balance').text(currentBalance);
   $tr.children('.cat').text(catEntry);
-
   // adding switch operator
   switch (catEntry) {
     case 'Bill':
@@ -59,18 +59,19 @@ function newTrans() {
     console.log(amountEntry);
     }
 
+  debugger;
   $('#registry').prepend($tr);
 }
 
-  function deleteTrans() {
-    $(this).closest('tr').remove();
-  }
-  function findCat(event) {
-    event.preventDefault();
-    var text = $(this).text();
-    $('#catEntry').text(text).append('<span class="caret"></span>');
-    console.log(text);
-  }
+function deleteTrans() {
+  $(this).closest('tr').remove();
+}
+function findCat(event) {
+  event.preventDefault();
+  var text = $(this).text();
+  $('#catEntry').text(text).append('<span class="caret"></span>');
+  console.log(text);
+}
 
 var currentBalance = 4230.17;
 //
@@ -86,6 +87,24 @@ var currentBalance = 4230.17;
 //     $('tbody').find('td').attr('selected', true);
 //     if$(this).val() !== 'none' {
 //       $('#catDropDown').val('');
+//     }
+//   }
+// }
+
+// function catFilter() {
+//   $('#catDropDown').change(function() {
+//     $('#selectedCat option').show();
+//     if($('option:selected', this).attr('id')) === 'selectedBill') {
+//       $('#selectedCat option.Bill').hide();
+//     }
+//     if($('option:selected', this).attr('id')) === 'selectionIncome') {
+//       $('#selectedCat option.Income')
+//     }
+//     if($('option:selected', this).attr('id')) === 'selectionEntertainment') {
+//       $('#selectedCat option.Entertainment')
+//     }
+//     if($('option:selected', this).attr('id')) === 'selectionSavings') {
+//       $('#selectedCat option.Savings')
 //     }
 //   }
 // }
