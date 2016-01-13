5c6f7a4ca0c0758f2c7062b547e0686f44cdfd38
Apple <tiffine.koch@gmail.com>
2016-01-13 12:46:02 -0800

    submitting assignment

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
-
 $(document).ready(init);
-
 
 function init() {
 
@@ -8,8 +6,7 @@ function init() {
   $('#formEntry').submit(newTrans);
   $('#catDropDown').on('change', findCat);
   $('table').on('click', '#trash', deleteTrans);
-  console.log('load');
-
+  // console.log('load');
 }
 
 function removeEntries() {
@@ -17,24 +14,29 @@ function removeEntries() {
 }
 
 function newTrans() {
-  console.log('hi');
-  console.log(this);
   event.preventDefault();
 
   var transName = $('#transEntry').val();
   var dateEntry = $('#dateEntry').val();
-  var formattedDate = moment(dateEntry).format('l');
+  var formattedDate = moment(dateEntry).format('llll');
   var amountEntry = $('#amountEntry').val();
-  // var currentBalance =
-  // var catEntry = $('ul.foo li:selected').val();
   var catEntry = $('#catEntry').text();
+
+  // need a conditional based on category - holds neg vs pos values
+  // var balance = $('li:nth-of-type(1)').val();
+  var balance = 4230.17;
+  // var newBalance = parseFloat(balance) - parseFloat(amountEntry);
+  var newBalance = balance - amountEntry;
+  console.log(newBalance);
+
 
   var $tr = $('#template').clone();
   $tr.removeAttr('id');
   $tr.children('.trans').text(transName);
   $tr.children('.date').text(formattedDate);
   var $newAmount = $tr.children('.amount');
-  // $tr.children('.balance').text(currentBalance);
+  $tr.children('.balance').text(newBalance);
+  // console.log('test');
   $tr.children('.cat').text(catEntry);
   // adding switch operator
   switch (catEntry) {
@@ -56,10 +58,9 @@ function newTrans() {
       break
     default:
       $newAmount.text('');
-    console.log(amountEntry);
+    // console.log(amountEntry);
     }
-
-  debugger;
+  // debugger;
   $('#registry').prepend($tr);
 }
 
@@ -73,38 +74,40 @@ function findCat(event) {
   console.log(text);
 }
 
-var currentBalance = 4230.17;
+//declare var currentBalance
+//turn this into a string
+//grab $newAmount and concat that
+//run eval(currentBalance)
+//return this
+
+// var currentBalance;
+// var balance = 4230.17;
 //
-// var entryObject = function(prop){
-//   this.trans = prop.trans;
-//   this.date = prop.date;
-//   this.amount = prop.amount;
-//   this.balance = prop.balance;
-//   this.cat = prop.cat;
-// }
-// function catFilter() {
-//   $('catDropDown').on('change', function(){
-//     $('tbody').find('td').attr('selected', true);
-//     if$(this).val() !== 'none' {
-//       $('#catDropDown').val('');
-//     }
-//   }
+// function findBalance( ) {
+//   var balance = 4230.17;
+//   balance = balance + nevValue;
+//   balance = balance.toString();
+//   $newAmount = $newAmount.toString();
+//   var currentBalance = balance.concat($newAmount);
+//   currentBalance = eval(currentBalance);
+//   console.log(currentBalance);
 // }
 
+//write switch operator here
 // function catFilter() {
 //   $('#catDropDown').change(function() {
-//     $('#selectedCat option').show();
-//     if($('option:selected', this).attr('id')) === 'selectedBill') {
-//       $('#selectedCat option.Bill').hide();
+//     $('#selectedCat li').show();
+//     if($('li:selected', this).attr('id')) === 'selected Bill') {
+//       $('#selectedCat li.Bill').hide();
 //     }
-//     if($('option:selected', this).attr('id')) === 'selectionIncome') {
-//       $('#selectedCat option.Income')
+//     if($('li:selected', this).attr('id')) === 'selected Income') {
+//       $('#selectedCat li.Income')
 //     }
-//     if($('option:selected', this).attr('id')) === 'selectionEntertainment') {
-//       $('#selectedCat option.Entertainment')
+//     if($('li:selected', this).attr('id')) === 'selected Entertainment') {
+//       $('#selectedCat li.Entertainment')
 //     }
-//     if($('option:selected', this).attr('id')) === 'selectionSavings') {
-//       $('#selectedCat option.Savings')
+//     if($('li:selected', this).attr('id')) === 'selected Savings') {
+//       $('#selectedCat li.Savings')
 //     }
 //   }
 // }
